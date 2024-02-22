831034c23ed78f9dccac4960b884553bbb927e24
Carolis <89231697+carolisc@users.noreply.github.com>
2024-02-22 19:52:56 -0300

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,50 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'plugin:react/jsx-runtime',
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react'
+  ],
+  'rules': {
+    'indent': [
+      'warn',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'windows'
+    ],
+    'quotes': [
+      'warn',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
+}
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import './App.module.scss'
+
+export function App() {
+
+  return (
+    <h1>MagBank</h1>
+  )
+}
